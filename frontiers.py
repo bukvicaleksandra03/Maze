50025b815be4276ca296aa30f755df3253adbd47
Aleksandra Bukvic <bukvic.aleksandra03@gmail.com>
2024-09-14 17:34:10 +0200

    First commit

---
+++ frontiers.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier():
+    def __init__(self):
+        self.frontier = deque()
+        
+    def add(self, node):
+        self.frontier.append(node)
+        
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+    
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier.popleft()
+            return node
---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import time
+from bfs_maze import BFSMaze
+from dfs_maze import DFSMaze
+from greedy_bfs_maze import GreedyBFSMaze
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python maze.py maze.txt")
+
+m = GreedyBFSMaze(sys.argv[1])
+# print("Maze:")
+# m.print()
+print("Solving...")
+
+start = time.time()
+m.solve()
+end = time.time()
+
+print(f"Time needed to find solution: {(end - start) * 1000}ms")
+print("States Explored:", m.num_explored)
+print("Solution:")
+m.print()
+m.output_image("maze.png", show_explored=True)
